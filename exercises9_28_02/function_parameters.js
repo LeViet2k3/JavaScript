843532aec404354dc22c1a9582247bc6a14012bf
Le Viet <viet.gm.2k3@gmail.com>
2024-03-08 13:52:14 +0700

    -update folder name

--- exercises9_28_02/function_parameters.js
+++ exercises9_28_02/function_parameters.js
@@ -18,8 +18,8 @@ func1(1, 2, 3)
 
 var person1 = { name: 'Van Viet', age: 19 };
 var person2 = { name: 'Le Viet', age: 20 };
-var sayHello = function () { alert('Hello, ' + this.name); };
-var sayGoodbye = function () { alert('Goodbye, ' + this.name); };
+var sayHello = function () { console.log('Hello, ' + this.name); };
+var sayGoodbye = function () { console.log('Goodbye, ' + this.name); };
 
 sayHello();
 sayGoodbye();
