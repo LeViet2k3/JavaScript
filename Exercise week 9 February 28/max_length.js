8baf89fceb52707d82a57c962d0e471e3c452e53
Le Viet <viet.gm.2k3@gmail.com>
2024-04-09 15:14:33 +0700

    -update fix bug file max_length.js

--- Exercise week 9 February 28/max_length.js
+++ Exercise week 9 February 28/max_length.js
@@ -1,6 +1,6 @@
 function longestWord(str) {
     const words = str.split(' ');
-    let longest = '';
+    let longest = words[0];
     for (let i = 0; i < words.length; i++) {
         if (words[i].length > longest.length) {
             longest = words[i];
